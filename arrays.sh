c16b81230d49664fdcce554869fe6b47798aaac9
Rachana <rachana100902@gmail.com>
2021-10-17 18:24:37 +0530

    added other commands

--- arrays.sh
+++ arrays.sh
@@ -8,4 +8,15 @@ echo "Enter name:"
 read  #note:we haven't used any variable here to take the input
 echo "Name: $REPLY"  #REPLY is inbuilt variable used as default variable
 
-echo $0 #$0 will give the script name
+echo $0 # $0 will give the script name
+
+os=('ubuntu' 'windows' 'mac' 'unix')
+echo "${os[@]}" # print all the os
+echo "${os[1]}" # print windows
+echo "${#os[@]}" # print the length of array os (@)
+
+os[3]='Kali' # third replaced by kali
+os[9]='linux'
+
+unset os[2]  # os at index 2 is removed
+echo "${os[@]}"
