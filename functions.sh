905fb0d51505bce200b2332a34404257d901a576
Rachana <rachana100902@gmail.com>
2021-10-17 20:13:36 +0530

    added function

---
+++ functions.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/bash
+
+# Syntax:
+# 1. 
+# function name_of_func ()
+# {
+#     commands
+# }
+
+# 2.
+# name_of_func()
+# {
+#     commands
+# }
+
+function hello()
+{
+    echo "Hello world"
+}
+
+quit()
+{
+    exit
+}
+
+hello
+quit
