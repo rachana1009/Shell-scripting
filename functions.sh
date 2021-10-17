c879dd6c207cbb4310017593183045b6fead8260
Rachana <rachana100902@gmail.com>
2021-10-17 20:21:11 +0530

    added function with local variable

--- functions.sh
+++ functions.sh
@@ -31,3 +31,16 @@ function print ()
     echo $1 $2
 }
 print am Rachana
+
+
+# by default script variables are local variables
+
+function lprint()
+{
+    local name=$1 
+    echo "Name is $name"
+}
+lprint Rachana
+#local variable value doesnot change global variable value of the same name
+
+
